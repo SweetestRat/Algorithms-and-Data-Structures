a6c414f2f89915da78014a46251f618998c5e9df
Vlada Gilde <61109457+SweetestRat@users.noreply.github.com>
2020-03-17 19:54:17 +0300

    Update D_GraphCondensation.cpp

--- 9 lab/D_GraphCondensation.cpp
+++ 9 lab/D_GraphCondensation.cpp
@@ -1,36 +1,36 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-
+ 
 using namespace std;
-
+ 
 ifstream fin ("cond.in");
 ofstream fout ("cond.out");
-
+ 
 vector<vector<int>> RealAdjList;
 vector<vector<int>> ReverseAdjList;
 vector<char> visited;
 vector<int> order, component;
 int componentPointer = 0;
-
+ 
 void dfs1(int vertex)
 {
     visited[vertex] = 1;
-
+ 
     for(int i = 0; i < RealAdjList[vertex].size(); ++i)
     {
         if(visited[RealAdjList[vertex][i]] == 0)
             dfs1(RealAdjList[vertex][i]);
     }
-
+ 
     order.push_back(vertex);
 }
-
+ 
 void dfs2(int vertex)
 {
     visited[vertex] = 1;
     component[vertex] = componentPointer;
-
+ 
     for(int i = 0; i < ReverseAdjList[vertex].size(); ++i)
     {
         if(visited[ReverseAdjList[vertex][i]] == 0)
@@ -39,25 +39,17 @@ void dfs2(int vertex)
         }
     }
 }
-
+ 
 int main()
 {
     int col1, col2;
     int numV, numE;
-
+ 
     fin >> numV >> numE;
-    if(numE == 0)
-    {
-        fout << numV << endl;
-        for(int i = 1; i <= numV; ++i)
-            fout << i << ' ';
-        return 0;
-    }
-
     RealAdjList.assign(numV + 1, vector<int>());
     ReverseAdjList.assign(numV + 1, vector<int>());
     component.assign(numV + 1, 0);
-
+ 
     for(int edge = 0; edge < numE; ++edge)
     {
         fin >> col1 >> col2;
@@ -67,16 +59,16 @@ int main()
             ReverseAdjList[col2].push_back(col1);
         }
     }
-
+ 
     visited.assign(numV + 1, 0);
     for(int i = 1; i <= numV; ++i)
     {
         if(visited[i] == 0)
             dfs1(i);
     }
-
+ 
     visited.assign(numV + 1, 0);
-    for(int i = order.size() - 1; i > 0; --i)
+    for(int i = order.size() - 1; i >= 0; --i)
     {
         if(visited[order[i]] == 0)
         {
@@ -84,9 +76,9 @@ int main()
             dfs2(order[i]);
         }
     }
-
+ 
     fout << componentPointer << endl;
-
+ 
     for(int i = 1; i <= numV; ++i)
     {
         fout << component[i] << ' ';
